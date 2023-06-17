1a26b01cb30adf46fd6ad5c1bdbc333134520f3d
demattosanthony <demattosanthony@gmail.com>
2023-06-17 10:38:09 -0400

    header turns into new header

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -21,7 +21,7 @@ export default function Landing() {
   const handleWheelScroll = (e: any) => {
     setScrollY((prev) => clamp(prev + e.deltaY, 0, 100))
   }
-  const headerPosition = clamp(scrollY, 5, 80)
+  const headerPosition = clamp(scrollY, 5, 45)
 
   const { y } = useSpring({
     from: { y: "5vh" },
@@ -65,30 +65,47 @@ export default function Landing() {
           ))}
       </div>
 
-      <animated.div
-        className="absolute bottom-[85px] left-5 lg:left-10 z-40"
-        style={{ bottom: y }}
-      >
-        {scrollY > 50 ? (
-          <animated.h1
-            className=" font-extrabold text-6xl md:text-8xl min-w-max flex"
-            style={{ opacity: opacity.to((o) => 1 - o) }}
-          >
-            this is netonomy
-          </animated.h1>
-        ) : (
-          <animated.h1
-            className=" font-extrabold text-6xl md:text-8xl min-w-max flex"
-            style={{ opacity: opacity.to((o) => o) }}
-          >
+      {scrollY > 50 ? (
+        <animated.div
+          className="absolute left-5 lg:left-10 z-4"
+          style={{ bottom: y, opacity: opacity.to((o) => 1 - o) }}
+        >
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-wrap text-5xl md:text-6xl xl:text-8xl">
+              <div className="font-light">This is&nbsp;</div>
+              <h1 className="font-extrabold min-w-max">netonomy</h1>
+            </div>
+
+            <p className="leading-7 [&:not(:first-child)]:mt-6 ml-2 max-w-xs sm:max-w-sm md:max-w-lg md:ml-4 xl:max-w-3xl">
+              Welcome to Netonomy, where we are redefining the way you control
+              your digital identity, data, and finances. With a powerful blend
+              of decentralization and blockchain technology, our upcoming wallet
+              application aims to empower individuals and organizations to
+              regain control of their digital lives. <br />
+              <br />
+              As we venture into an era of digital sovereignty, we invite you to
+              join us on this journey. Stay tuned for updates as we bring this
+              vision to life, empowering you to navigate the digital world with
+              privacy, control, and peace of mind.
+            </p>
+
+            <Button className="w-52 ml-4 mt-6">Explore the Platform</Button>
+          </div>
+        </animated.div>
+      ) : (
+        <animated.div
+          className="absolute  left-5 lg:left-10 z-40"
+          style={{ bottom: y, opacity: opacity.to((o) => o) }}
+        >
+          <animated.h1 className=" font-extrabold text-6xl md:text-8xl min-w-max flex">
             netonomy
           </animated.h1>
-        )}
 
-        <p className="leading-7 [&:not(:first-child)] max-w-xs sm:max-w-sm md:max-w-lg xl:max-w-3xl">
-          Your data stored by you for you
-        </p>
-      </animated.div>
+          <p className="leading-7 [&:not(:first-child)] max-w-xs sm:max-w-sm md:max-w-lg xl:max-w-3xl">
+            Your data stored by you for you
+          </p>
+        </animated.div>
+      )}
 
       <div className="flex absolute top-[85px] right-6 flex-col ">
         <Button
